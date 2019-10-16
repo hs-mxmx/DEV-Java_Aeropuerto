430ae02f968e74d85f3f21013011cabb607586ae
Berbel <52207784+BerbelStyle@users.noreply.github.com>
2019-10-16 23:20:19 +0200

    Add files via upload

---
+++ PEL1_Aeropuerto/src/pel1_aeropuerto/Empleado.java
@@ -0,0 +1,29 @@
+package aeropuerto;
+
+public class Empleado extends Thread {
+ 
+    private Cinta miCinta;
+    private String id;
+    
+    public Empleado(String id, Cinta miCinta)
+    {
+        this.miCinta=miCinta;
+        this.id=id;
+    }
+    
+    public void setID(String nombre) {
+        this.id=nombre;
+    }
+    
+    @Override
+    public void run()
+    {
+        while(true) {
+            try
+            {
+                sleep((int)(400+300*Math.random()));
+            } catch(InterruptedException e){ }
+                miCinta.recogerMaleta(id);
+        }
+    }
+}
